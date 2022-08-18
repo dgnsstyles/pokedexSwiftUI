eaa7516a60d04fccf01277b54634b29315d6d93a
David Goren <dgoren@hotmail.com>
2022-08-18 17:49:30 -0400

    Initial Commit

---
+++ pokedexSwiftUI/pokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexSwiftUIApp.swift
+//  pokedexSwiftUI
+//
+//  Created by David Goren on 18-08-22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
